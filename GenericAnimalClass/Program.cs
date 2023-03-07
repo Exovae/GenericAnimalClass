905e9e77d63c419bbee6b5b194c6b8392bdfbe64
Exovae <elliot@keymetricsoft.com>
2023-03-06 19:38:27 -0500

    Add project files.

---
+++ GenericAnimalClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Animal<T>
+{
+    private T data;
+
+    public Animal(T data)
+    {
+        this.data = data;
+    }
+
+    public T getAnimal()
+    {
+        return data;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // create Animal object with string, string, bool, bool, and int
+        Animal<Tuple<string, string, bool, bool, int>> animal = new Animal<Tuple<string, string, bool, bool, int>>(
+            new Tuple<string, string, bool, bool, int>("lion", "grasslands", false, false, 15));
+
+        // print out values using getAnimal method
+        Tuple<string, string, bool, bool, int> animalData = animal.getAnimal();
+        Console.WriteLine("Name: {0}", animalData.Item1);
+        Console.WriteLine("Habitat: {0}", animalData.Item2);
+        Console.WriteLine("Endangered: {0}", animalData.Item3);
+        Console.WriteLine("Extinct: {0}", animalData.Item4);
+        Console.WriteLine("Average Lifespan: {0}", animalData.Item5);
+    }
+}
